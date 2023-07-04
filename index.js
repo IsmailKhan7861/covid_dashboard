7729557077e79c2e03fd032580056e9edb95de1d
ismail.k <ismail.k@internal.media.net>
2023-07-04 15:04:28 +0530

    updated

---
+++ index.js
@@ -0,0 +1,34 @@
+//TOTAL WORLD DATA
+//COMPARE WITH
+
+let totalCases = document.getElementById("total-cases");
+let totalRecovered = document.getElementById("total-recovered");
+let totalActive = document.getElementById("total-active");
+let totalDeaths = document.getElementById("total-deaths");
+let nan = document.getElementById("nan");
+
+//Total Cases fetch call and data addtion
+async function totalData() {
+  // let url = "https://api.rootnet.in/covid19-in/stats/latest";
+  let url = "https://disease.sh/v3/covid-19/all";
+  const data = await fetch(url);
+  const jsonData = await data.json();
+
+  fillData(jsonData);
+}
+//changing the text
+const fillData = (jsonData) => {
+  totalCases.textContent = jsonData.cases;
+  totalDeaths.textContent = jsonData.deaths;
+  totalRecovered.textContent = jsonData.recovered;
+  totalActive.textContent = jsonData.active;
+  nan.textContent =
+    Math.floor((totalRecovered.textContent / totalCases.textContent) * 100) +
+    "%";
+};
+totalData();
+
+//countrywise
+//Twiiter
+//map for country wise
+//affected areas
